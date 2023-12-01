7cc07a7718f6db676259d17135d2f423b3bd224d
Ian Quinto <codedianq@gmail.com>
2023-12-01 17:27:44 +0800

    Add function to get user input and store in object

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 // Add function that take user's input and store to array.
 const myLibrary = [];
 
-function Book() {}
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
 
-function addBookToLibrary() {}
+function addBookToLibrary() {
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+  let pages = document.querySelector('#pages').value;
+  let read = document.querySelector('#read').checked;
+  const newBook = new Book(title, author, pages, read);
+  console.log(newBook);
+}
 
 // New book button
 const newBookBtn = document.querySelector('#new-book-btn');
@@ -11,3 +23,10 @@ newBookBtn.addEventListener('click', function () {
   const formContainer = document.querySelector('.form-container');
   formContainer.style.display = 'block';
 });
+
+document
+  .querySelector('#new-book-form')
+  .addEventListener('submit', function () {
+    event.preventDefault();
+    addBookToLibrary();
+  });
